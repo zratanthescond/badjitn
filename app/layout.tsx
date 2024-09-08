510d8b634232d16f5ca3b68df0e2da14ca16e55a
zratanthescond <honcongs1@gmail.com>
2024-09-08 15:02:29 +0100

    title

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,10 +12,10 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: "Evently",
-  description: "Evently is a platform for event management.",
+  title: "BadjiTn",
+  description: "BadjiTn is a platform for event management.",
   icons: {
-    icon: "/assets/images/logo.svg",
+    icon: "/assets/images/logo.png",
   },
 };
 
