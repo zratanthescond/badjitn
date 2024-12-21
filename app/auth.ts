ccd8f1459da9fb655fe519fb2dd0621aa59c4f03
zratanthescond <honcongs1@gmail.com>
2024-12-21 19:44:43 +0100

    roll back

--- app/auth.ts
+++ app/auth.ts
@@ -1,49 +1,100 @@
 import NextAuth from "next-auth";
-import "next-auth/jwt";
+import GoogleProvider from "next-auth/providers/google";
+import FacebookProvider from "next-auth/providers/facebook";
+import twitter from "next-auth/providers/twitter";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
 
-import Facebook from "next-auth/providers/facebook";
+import { MongoClient } from "mongodb";
+import { IUser } from "@/lib/database/models/user.model";
+/**
+ * Takes a token, and returns a new token with updated
+ * `accessToken` and `accessTokenExpires`. If an error occurs,
+ * returns the old token and an error property
+ */
 
-import Google from "next-auth/providers/google";
+const client = new MongoClient(process.env.MONGODB_URI || "");
+const clientPromise = client.connect();
 
-import Twitter from "next-auth/providers/twitter";
+const authOptions = {
+  adapter: MongoDBAdapter(clientPromise),
+  providers: [
+    CredentialsProvider({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      async authorize(credentials) {
+        console.log(credentials);
+        if (credentials === null) return null;
 
-export const { handlers, auth, signIn, signOut } = NextAuth({
-  debug: !!process.env.AUTH_DEBUG,
-  theme: { logo: "https://authjs.dev/img/logo-sm.png" },
+        try {
+          const user: IUser = await authOptions?.adapter?.getUserByEmail(
+            credentials.email
+          );
+          console.log("user", user);
 
-  providers: [Facebook, Google, Twitter],
-  //basePath: "api/auth",
-  session: { strategy: "jwt" },
+          if (!user) {
+            return null;
+          }
+          return user;
+        } catch (e) {
+          console.log("******************************************************");
+          console.error(e);
+          console.log("******************************************************");
+          return null;
+        }
+      },
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+    FacebookProvider({
+      clientId: process.env.AUTH_FACEBOOK_ID,
+      clientSecret: process.env.AUTH_FACEBOOK_SECRET,
+    }),
+    twitter({
+      clientId: process.env.AUTH_TWITTER_ID,
+      clientSecret: process.env.AUTH_TWITTER_SECRET,
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+    maxAge: 24 * 60 * 60,
+    updateAge: 24 * 60 * 60,
+  },
   callbacks: {
-    authorized({ request, auth }) {
-      const { pathname } = request.nextUrl;
-      if (pathname === "/middleware-example") return !!auth;
-      return true;
-    },
-    jwt({ token, trigger, session, account }) {
-      if (trigger === "update") token.name = session.user.name;
-      if (account?.provider === "keycloak") {
-        return { ...token, accessToken: account.access_token };
+    jwt: async ({ token, user, account }) => {
+      console.log(account);
+      if (user) {
+        token.uid = user;
       }
+
       return token;
     },
-    async session({ session, token }) {
-      if (token?.accessToken) session.accessToken = token.accessToken;
+    session: async ({ session, token }) => {
+      // here we put session.useData and put inside it whatever you want to be in the session
+      // here try to console.log(token) and see what it will have
+      // sometimes the user get stored in token.uid.userData
+      // sometimes the user data get stored in just token.uid
+      //console.log("account", account);
+      //console.log("token", token);
+      token.uid.password = null;
+      session.userData = token.uid;
 
       return session;
     },
   },
-  experimental: { enableWebAuthn: true },
-});
 
-declare module "next-auth" {
-  interface Session {
-    accessToken?: string;
-  }
-}
+  secret: process.env.AUTH_SECRET,
+};
 
-declare module "next-auth/jwt" {
-  interface JWT {
-    accessToken?: string;
-  }
-}
+export const { handlers, signIn, signOut, auth } = NextAuth(authOptions);
