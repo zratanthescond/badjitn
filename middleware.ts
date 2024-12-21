f8e6b639537946b5f7c45998f414a788431c1345
zratanthescond <honcongs1@gmail.com>
2024-12-21 18:19:29 +0100

    bchall1

--- middleware.ts
+++ middleware.ts
@@ -1,44 +1,41 @@
-// import { NextRequest, NextResponse } from "next/server";
-// import { authConfig } from "./app/auth.config";
-// import NextAuth from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+import { authConfig } from "./app/auth.config";
+import NextAuth from "next-auth";
 
-// const { auth } = NextAuth(authConfig);
+//const { auth } = NextAuth(authConfig);
 
-// import {
-//   PUBLIC_ROUTES,
-//   LOGIN,
-//   ROOT,
-//   PROTECTED_SUB_ROUTES,
-//   SIGN_UP,
-// } from "./lib/route";
+import {
+  PUBLIC_ROUTES,
+  LOGIN,
+  ROOT,
+  PROTECTED_SUB_ROUTES,
+  SIGN_UP,
+} from "./lib/route";
 
-// export async function middleware(request: NextRequest) {
-//   const { nextUrl } = request;
-//   const session = await auth();
-//   console.log(session);
-//   const isAuthenticated = !!session?.user;
-//   console.log(isAuthenticated, nextUrl.pathname);
+export async function middleware(request: NextRequest) {
+  // const { nextUrl } = request;
+  // const session = await auth();
+  // console.log(session);
+  // const isAuthenticated = !!session?.user;
+  // console.log(isAuthenticated, nextUrl.pathname);
+  // const isPublicRoute =
+  //   (PUBLIC_ROUTES.find((route: string) =>
+  //     nextUrl.pathname.startsWith(route)
+  //   ) ||
+  //     nextUrl.pathname === ROOT) &&
+  //   !PROTECTED_SUB_ROUTES.find((route: string) =>
+  //     nextUrl.pathname.includes(route)
+  //   );
+  // console.log(isPublicRoute);
+  // if (!isAuthenticated && !isPublicRoute)
+  //   return Response.redirect(new URL(LOGIN, nextUrl));
+  // if (
+  //   isAuthenticated &&
+  //   (nextUrl.pathname.startsWith(SIGN_UP) || nextUrl.pathname.startsWith(LOGIN))
+  // )
+  //   return NextResponse.rewrite(new URL(ROOT, nextUrl));
+}
 
-//   const isPublicRoute =
-//     (PUBLIC_ROUTES.find((route: string) =>
-//       nextUrl.pathname.startsWith(route)
-//     ) ||
-//       nextUrl.pathname === ROOT) &&
-//     !PROTECTED_SUB_ROUTES.find((route: string) =>
-//       nextUrl.pathname.includes(route)
-//     );
-
-//   console.log(isPublicRoute);
-
-//   if (!isAuthenticated && !isPublicRoute)
-//     return Response.redirect(new URL(LOGIN, nextUrl));
-//   if (
-//     isAuthenticated &&
-//     (nextUrl.pathname.startsWith(SIGN_UP) || nextUrl.pathname.startsWith(LOGIN))
-//   )
-//     return NextResponse.rewrite(new URL(ROOT, nextUrl));
-// }
-
-// export const config = {
-//   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-// };
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
