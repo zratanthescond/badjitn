ea47f8db58c4d3b286fb951c19b161128ec14096
zratanthescond <honcongs1@gmail.com>
2024-12-26 07:04:03 +0100

    sf

--- app/api/music/route.ts
+++ app/api/music/route.ts
@@ -1,7 +1,12 @@
 import { connectToDatabase } from "@/lib/database";
 import Music from "@/lib/database/models/music.model";
+import { handleError } from "@/lib/utils";
 export async function GET(req: Request, res: Response) {
-  await connectToDatabase();
-  const musics = await Music.find({});
-  return new Response(JSON.stringify(musics), { status: 200 });
+  try {
+    await connectToDatabase();
+    const musics = await Music.find({});
+    return new Response(JSON.stringify(musics), { status: 200 });
+  } catch (error) {
+    handleError(error);
+  }
 }
