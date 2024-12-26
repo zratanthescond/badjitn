dd4014668765f6879cc665d194bc491ec6852573
zratanthescond <honcongs1@gmail.com>
2024-12-26 20:16:32 +0100

    hghhg

--- app/api/music/route.ts
+++ app/api/music/route.ts
@@ -1,12 +1,11 @@
 import { connectToDatabase } from "@/lib/database";
 import Music from "@/lib/database/models/music.model";
 import { handleError } from "@/lib/utils";
-import { revalidatePath } from "next/cache";
+export const dynamic = "force-dynamic";
 export async function GET(req: Request, res: Response) {
   try {
     await connectToDatabase();
     const musics = await Music.find({});
-    revalidatePath(req.url);
     return new Response(JSON.stringify(musics), { status: 200 });
   } catch (error) {
     handleError(error);
