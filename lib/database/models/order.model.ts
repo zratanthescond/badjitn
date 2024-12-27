7680bb35cc8ccff9f5f527f00573c10df4b83b0f
zratanthescond <honcongs1@gmail.com>
2024-12-27 06:35:04 +0100

    dfgd

--- lib/database/models/order.model.ts
+++ lib/database/models/order.model.ts
@@ -1,30 +1,51 @@
-import { Schema, model, models, Document } from 'mongoose'
+import { Schema, model, models, Document } from "mongoose";
+
+enum OrderType {
+  PAID = "paid",
+  HOTED = "hoted",
+}
+
+export type Detail = {
+  name: string;
+  price: string;
+};
 
 export interface IOrder extends Document {
-  createdAt: Date
-  stripeId: string
-  totalAmount: string
+  createdAt: Date;
+  stripeId: string;
+  totalAmount: string;
   event: {
-    _id: string
-    title: string
-  }
+    _id: string;
+    title: string;
+  };
   buyer: {
-    _id: string
-    firstName: string
-    lastName: string
-  }
+    _id: string;
+    firstName: string;
+    lastName: string;
+  };
+  type: OrderType;
+  details: Detail[];
 }
 
 export type IOrderItem = {
-  _id: string
-  totalAmount: string
-  createdAt: Date
-  eventTitle: string
-  eventId: string
-  buyer: string
-}
+  _id: string;
+  totalAmount: string;
+  createdAt: Date;
+  eventTitle: string;
+  eventId: string;
+  buyer: string;
+  type: OrderType;
+  details: Detail[];
+};
 
-const OrderSchema = new Schema({
+// Subdocument schema for `Detail`
+const DetailSchema = new Schema<Detail>({
+  name: { type: String, required: true },
+  price: { type: String, required: true },
+});
+
+// Main schema for `Order`
+const OrderSchema = new Schema<IOrder>({
   createdAt: {
     type: Date,
     default: Date.now,
@@ -39,14 +60,27 @@ const OrderSchema = new Schema({
   },
   event: {
     type: Schema.Types.ObjectId,
-    ref: 'Event',
+    ref: "Event",
+    required: true,
   },
   buyer: {
     type: Schema.Types.ObjectId,
-    ref: 'User',
+    ref: "User",
+    required: true,
   },
-})
+  type: {
+    type: String,
+    enum: Object.values(OrderType),
+    default: OrderType.PAID,
+    required: true,
+  },
+  details: {
+    type: [DetailSchema],
+    required: true,
+    default: [],
+  },
+});
 
-const Order = models.Order || model('Order', OrderSchema)
+const Order = models.Order || model<IOrder>("Order", OrderSchema);
 
-export default Order
+export default Order;
