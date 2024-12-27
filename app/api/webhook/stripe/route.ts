5fd65af41d6abcc79eec2a8cd7b496494c4f1e27
zratanthescond <honcongs1@gmail.com>
2024-12-27 04:35:10 +0100

    ggg

--- app/api/webhook/stripe/route.ts
+++ app/api/webhook/stripe/route.ts
@@ -9,10 +9,11 @@ export async function POST(request: Request) {
   const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET!;
 
   let event;
-
+  console.log("stripe webhook called");
   try {
     event = stripe.webhooks.constructEvent(body, sig, endpointSecret);
   } catch (err) {
+    console.log(err);
     return NextResponse.json({ message: "Webhook error", error: err });
   }
 
