4b7be5ecc7475635f7456a3abd042dc8d43ff219
zratanthescond <honcongs1@gmail.com>
2025-03-22 21:57:43 +0100

    scrool button

--- components/shared/ScrollShorts.tsx
+++ components/shared/ScrollShorts.tsx
@@ -21,7 +21,9 @@ import ReelDetails from "../ReelDetails";
 import { Event } from "@/types";
 import {
   ArrowBigLeft,
+  ArrowDown,
   ArrowLeft,
+  ArrowUp,
   Facebook,
   Globe,
   Heart,
@@ -76,6 +78,23 @@ const ShortsScroll = ({ videos }: { videos?: Props }) => {
   const videoRefs = useRef<(HTMLVideoElement | null)[]>([]);
   const videosData = useMemo(() => videos.data, [videos.data]);
   const Router = useRouter();
+
+  const scrollToNext = () => {
+    if (parentRef.current) {
+      const scrollDistance = parentRef.current.clientHeight; // Height of one full screen
+      parentRef.current.scrollBy({ top: scrollDistance, behavior: "smooth" });
+    }
+  };
+
+  const scrollToPrevious = () => {
+    if (parentRef.current) {
+      const scrollDistance = parentRef.current.clientHeight;
+      parentRef.current.scrollBy({
+        top: -scrollDistance,
+        behavior: "smooth",
+      });
+    }
+  };
   useEffect(() => {
     const observer = new IntersectionObserver(
       (entries) => {
@@ -113,135 +132,136 @@ const ShortsScroll = ({ videos }: { videos?: Props }) => {
   }, [videosData, parentRef.current]);
 
   return (
-    <div
-      ref={parentRef}
-      className="shorts-scroll-container [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]"
-      style={{
-        height: "100vh",
-        overflowY: "scroll",
-        scrollSnapType: "y mandatory",
-        position: "relative",
-        width: "100vw",
-      }}
-    >
-      <div className="relative w-full ">
-        {rowVirtualizer.getVirtualItems().map((virtualItem) => {
-          const video = videos?.data[virtualItem.index];
+    <>
+      <div
+        ref={parentRef}
+        className="shorts-scroll-container [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]"
+        style={{
+          height: "100vh",
+          overflowY: "scroll",
+          scrollSnapType: "y mandatory",
+          position: "relative",
+          width: "100vw",
+        }}
+      >
+        <div className="relative w-full ">
+          {rowVirtualizer.getVirtualItems().map((virtualItem) => {
+            const video = videos?.data[virtualItem.index];
 
-          return (
-            <div
-              key={video._id}
-              className="h-screen  snap-start flex items-center justify-end gap-2 md:p-4 md:m-4 "
-            >
-              <div className="flex h-full w-full md:w-1/4 lg:w-1/4">
-                <HLSPlayer
-                  manifest={video.imageUrl}
-                  ref={(el) => (videoRefs.current[virtualItem.index] = el)}
-                  autoPlay
-                  loop
-                  muted
-                  controls
-                  className="md:rounded-xl object-fill w-full h-full   "
-                />
-                <div className="flex h-screen pointer-events-none justify-between w-full absolute flex-col p-2 md:w-1/4">
-                  <Button
-                    size={"icon"}
-                    className="glass rounded-full  pointer-events-auto"
-                    onClick={() => {
-                      Router.push("/");
-                    }}
-                  >
-                    <ArrowLeft color="white" />
-                  </Button>
-                  <div className="pr-3 max-h-fit max-w-fit self-end justify-self-end items-center pointer-events-auto justify-evenly gap-3 flex flex-col">
+            return (
+              <div
+                key={video._id}
+                className="h-screen  snap-start flex items-center justify-end gap-2 md:p-4 md:m-4 "
+              >
+                <div className="flex h-full w-full md:w-1/4 lg:w-1/4">
+                  <HLSPlayer
+                    manifest={video.imageUrl}
+                    ref={(el) => (videoRefs.current[virtualItem.index] = el)}
+                    autoPlay
+                    loop
+                    muted
+                    controls
+                    className="md:rounded-xl object-fill w-full h-full   "
+                  />
+                  <div className="flex h-screen pointer-events-none justify-between w-full absolute flex-col p-2 md:w-1/4">
                     <Button
                       size={"icon"}
-                      className="glass rounded-full "
+                      className="glass rounded-full  pointer-events-auto"
                       onClick={() => {
-                        setShow(!show);
+                        Router.push("/");
                       }}
                     >
-                      <List color="white" />
+                      <ArrowLeft color="white" />
                     </Button>
-                    <Button size={"icon"} className="glass rounded-full ">
-                      <Heart color="white" />
-                    </Button>
-                    <AlertDialog>
-                      <AlertDialogTrigger>
-                        <Button size={"icon"} className="glass rounded-full ">
-                          <Share2Icon color="white" />
-                        </Button>
-                      </AlertDialogTrigger>
-                      <AlertDialogContent>
-                        <div className="glass flex flex-col w-full p-5 gap-3 rounded-lg">
-                          <Input
-                            type="url"
-                            value={`${process.env.NEXT_PUBLIC_SERVER_URL}/events/${video._id}`}
-                          />
-                          <Button className="glass rounded-lg text-white">
-                            <Facebook color="white" />
-                            Facebook
+                    <div className="pr-3 max-h-fit max-w-fit self-end justify-self-end items-center pointer-events-auto justify-evenly gap-3 flex flex-col">
+                      <Button
+                        size={"icon"}
+                        className="glass rounded-full "
+                        onClick={() => {
+                          setShow(!show);
+                        }}
+                      >
+                        <List color="white" />
+                      </Button>
+                      <Button size={"icon"} className="glass rounded-full ">
+                        <Heart color="white" />
+                      </Button>
+                      <AlertDialog>
+                        <AlertDialogTrigger>
+                          <Button size={"icon"} className="glass rounded-full ">
+                            <Share2Icon color="white" />
                           </Button>
-                          <Button className="glass rounded-lg text-white">
-                            <Twitter color="white" />
-                            X/Twitter
-                          </Button>
-                          <Button className="glass rounded-lg text-white">
-                            <Instagram color="white" />
-                            Instagram
-                          </Button>
-                          <Button className="glass rounded-lg text-white">
-                            <FaWhatsapp color="white" size={25} />
-                            Whatsapp
-                          </Button>
-                          <div className="flex flex-row items-center justify-evenly">
-                            <AlertDialogCancel>Cancel</AlertDialogCancel>
-                            <AlertDialogAction>OK</AlertDialogAction>
+                        </AlertDialogTrigger>
+                        <AlertDialogContent>
+                          <div className="glass flex flex-col w-full p-5 gap-3 rounded-lg">
+                            <Input
+                              type="url"
+                              value={`${process.env.NEXT_PUBLIC_SERVER_URL}/events/${video._id}`}
+                            />
+                            <Button className="glass rounded-lg text-white">
+                              <Facebook color="white" />
+                              Facebook
+                            </Button>
+                            <Button className="glass rounded-lg text-white">
+                              <Twitter color="white" />
+                              X/Twitter
+                            </Button>
+                            <Button className="glass rounded-lg text-white">
+                              <Instagram color="white" />
+                              Instagram
+                            </Button>
+                            <Button className="glass rounded-lg text-white">
+                              <FaWhatsapp color="white" size={25} />
+                              Whatsapp
+                            </Button>
+                            <div className="flex flex-row items-center justify-evenly">
+                              <AlertDialogCancel>Cancel</AlertDialogCancel>
+                              <AlertDialogAction>OK</AlertDialogAction>
+                            </div>
                           </div>
-                        </div>
-                      </AlertDialogContent>
-                    </AlertDialog>
+                        </AlertDialogContent>
+                      </AlertDialog>
+                    </div>
+                    <h4 className="text-white pb-10">{video.title}</h4>
                   </div>
-                  <h4 className="text-white pb-10">{video.title}</h4>
-                </div>
-              </div>{" "}
-              <div
-                className={`${
-                  show
-                    ? "flex top-[30vh] bg-card/5 md:top-auto max-h-[70vh] md:max-h-full"
-                    : "hidden  md:flex"
-                } flex-col glass  fixed bottom-0  md:max-w-prose md:relative  rounded-2xl w-full  md:h-full items-center justify-start gap-2   animate-accordion-down repeat-1`}
-              >
-                <Button
-                  size={"icon"}
-                  variant={"ghost"}
-                  className=" absolute top-0 right-0 rounded-full z-10 md:hidden"
-                  onClick={() => {
-                    setShow(!show);
-                  }}
+                </div>{" "}
+                <div
+                  className={`${
+                    show
+                      ? "flex top-[30vh] bg-card/5 md:top-auto max-h-[70vh] md:max-h-full"
+                      : "hidden  md:flex"
+                  } flex-col glass  fixed bottom-0  md:max-w-prose md:relative  rounded-2xl w-full  md:h-full items-center justify-start gap-2   animate-accordion-down repeat-1`}
                 >
-                  <IoClose size={25} strokeWidth={25} color="white" />
-                </Button>
-                <ScrollArea className="w-full h-full">
-                  <div className="flex flex-row items-start justify-between  w-full p-2 ">
-                    <XProfileCard
-                      username={
-                        video.organizer.firstName +
-                        " " +
-                        video.organizer.lastName
-                      }
-                      avatarUrl={video.organizer.photo}
-                      isVerified={
-                        video.organizer.publisher &&
-                        video.organizer.publisher === "approved"
-                      }
-                      handle={video.organizer.username}
-                      organization={video.organizer.organisationName}
-                      bio={video.organizer.organisationDescription}
-                    />
-                  </div>
-                  <div className="flex flex-row items-center justify-between p-2">
-                    {/* {sponsors && !isPending && sponsors.data && (
+                  <Button
+                    size={"icon"}
+                    variant={"ghost"}
+                    className=" absolute top-0 right-0 rounded-full z-10 md:hidden"
+                    onClick={() => {
+                      setShow(!show);
+                    }}
+                  >
+                    <IoClose size={25} strokeWidth={25} color="white" />
+                  </Button>
+                  <ScrollArea className="w-full h-full">
+                    <div className="flex flex-row items-start justify-between  w-full p-2 ">
+                      <XProfileCard
+                        username={
+                          video.organizer.firstName +
+                          " " +
+                          video.organizer.lastName
+                        }
+                        avatarUrl={video.organizer.photo}
+                        isVerified={
+                          video.organizer.publisher &&
+                          video.organizer.publisher === "approved"
+                        }
+                        handle={video.organizer.username}
+                        organization={video.organizer.organisationName}
+                        bio={video.organizer.organisationDescription}
+                      />
+                    </div>
+                    <div className="flex flex-row items-center justify-between p-2">
+                      {/* {sponsors && !isPending && sponsors.data && (
                           // sponsors.data.map((card, index) => (
                           //   <div
                           //     key={index}
@@ -271,46 +291,65 @@ const ShortsScroll = ({ videos }: { videos?: Props }) => {
                           // ))}
                         
                         )} /*/}{" "}
-                    {video.sponsors && video.sponsors.length > 0 && (
-                      <SponsorsSection sponsorsIds={video.sponsors} />
-                    )}
-                  </div>
-                  <h1 className="font-semibold text-center w-full text-3xl py-4">
-                    {video.title}
-                  </h1>
-                  <div className="flex flex-row items-center justify-between p-2">
-                    <div className="flex gap-2 mb-4">
-                      <Badge className="bg-pink-500 hover:bg-pink-600">
-                        {video.category.name}
-                      </Badge>
+                      {video.sponsors && video.sponsors.length > 0 && (
+                        <SponsorsSection sponsorsIds={video.sponsors} />
+                      )}
                     </div>
-                    <div className="flex gap-2 mb-4">
-                      <a
-                        href={video.url || "#"}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="text-link mt-0.5"
-                      >
-                        <Button
-                          variant={"outline"}
-                          size={"sm"}
-                          className=" text-sm flex flex-row rounded-full items-center"
+                    <h1 className="font-semibold text-center w-full text-3xl py-4">
+                      {video.title}
+                    </h1>
+                    <div className="flex flex-row items-center justify-between p-2">
+                      <div className="flex gap-2 mb-4">
+                        <Badge className="bg-pink-500 hover:bg-pink-600">
+                          {video.category.name}
+                        </Badge>
+                      </div>
+                      <div className="flex gap-2 mb-4">
+                        <a
+                          href={video.url || "#"}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="text-link mt-0.5"
                         >
-                          <Globe className="mr-1 h-4 w-4" />
-                          Check website
-                        </Button>
-                      </a>
+                          <Button
+                            variant={"outline"}
+                            size={"sm"}
+                            className=" text-sm flex flex-row rounded-full items-center"
+                          >
+                            <Globe className="mr-1 h-4 w-4" />
+                            Check website
+                          </Button>
+                        </a>
+                      </div>
                     </div>
-                  </div>
-                  <ReelDetails event={video} />
-                  <ScrollBar orientation="vertical" />
-                </ScrollArea>
+                    <ReelDetails event={video} />
+                    <ScrollBar orientation="vertical" />
+                  </ScrollArea>
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
-    </div>
+      <div className="fixed hidden md:flex mx-2 h-screen top-0 right-0   flex-col items-center justify-center gap-2 p-2">
+        <Button
+          variant={"outline"}
+          size={"icon"}
+          onClick={scrollToPrevious}
+          className="rounded-full"
+        >
+          <ArrowUp className="h-10 w-10" />
+        </Button>
+        <Button
+          variant={"outline"}
+          size={"icon"}
+          onClick={scrollToNext}
+          className="rounded-full"
+        >
+          <ArrowDown className="h-10 w-10" />
+        </Button>
+      </div>
+    </>
   );
 };
 
